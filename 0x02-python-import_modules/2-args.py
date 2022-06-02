54733edcb4da2d6c1b944f85dbeec81ca90fa87b
xomcodes <xommyb@gmail.com>
2022-06-02 23:46:36 +0100

    Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,16 @@
-iiiiiii
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arg_count = len(argv)
+    index = 1
+    if arg_count == 0:
+        print("{:d} arguments.".format(arg_count))
+    elif arg_count == 1:
+        print("{:d} argument:".format(arg_count))
+        print("{:d}: {:s}".format(arg_count, sys.argv[index]))
+    else:
+        print("{:d} arguments:".format(arg_count))
+        while index <= arg_count:
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+            index += 1
