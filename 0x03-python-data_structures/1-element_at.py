f0080a11afb89e38df4fa5d111839f3149f0cfac
chisom okeoma <xommyb@gmail.com>
2022-06-06 15:17:19 -0700

    data structure

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    my_list_len = len(my_list)
+    i = 0
+    if idx < 0:
+        return (None)
+    elif idx >= my_list_len:
+        return (None)
+    else:
+        while i < my_list_len:
+            if idx == i:
+                return (my_list[i]
+                        i += 1
