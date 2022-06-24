26c92cbbc71027d713e061506f2238a86f21a4bb
xomcodes <xommyb@gmail.com>
2022-06-24 22:50:48 +0100

    task6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+        raises a name exception with a message
+        Args:
+            messsage: The exception message
+    """
+    raise NameError(message)
